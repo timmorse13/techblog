78ac78c0277d914c6ac85c0cf6c05cba842c3b10
timmorse13 <timmorse13@gmail.com>
2021-04-04 18:31:25 -0700

    set up routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,30 @@
+const routeer = require('express').Router();
+const { User, Submission } = require('../models');
+
+router.get('/', async (req, res) => {
+    res.render('homepage')
+});
+
+router.get('/submission/:id', async (req, res) => {
+    try {
+        const submissionId = await Submission.findByPk(req.params.id);
+        const subId = submissionId.get({ plain: true });
+        // res.render('', subId)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/profile', async (req, res) => {
+    try {
+        const profileData = await User.findByPk(req.session.user_id, {
+            include: [{ model: Submission }]
+        });
+        const profile = profileData.get({ plain: true });
+        res.render('profile', profile);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
